aaa8676909b3a7f6d764ae5e1856fb76d17bc6a2
sevivon <sevivon64@gmail.com>
2017-06-21 04:14:29 +0200

    correction détection incrémentation/décrémentation sur Firefox

--- minecraft/js/damier.js
+++ minecraft/js/damier.js
@@ -47,7 +47,7 @@ function genererFonction()
 $(document).ready(function()
 {
 	initialiserFormulaire();
-	$("#largeur, #hauteur").blur(verifierDimensions);
+	$("#largeur, #hauteur").on("input",verifierDimensions);
 	$("#boutonGenerer").click(genererFonction);
 	new Clipboard("#boutonCopier");
 });
